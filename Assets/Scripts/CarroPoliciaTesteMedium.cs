5b2acfe06c71b530f2461a2aeeea194ec7c1ae3c
LeKeu <lekeuffer@gmail.com>
2022-04-30 10:33:04 -0300

    ajeitei algumas coisas + spacebar restart

--- Assets/Scripts/CarroPoliciaTesteMedium.cs
+++ Assets/Scripts/CarroPoliciaTesteMedium.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class CarroPoliciaTesteMedium : MonoBehaviour
 {
-    public WinLoose WinLooseScript;
+    public WinLoose WinLooseScript; // to meio q importando esse script p utilizar as funções dele
     [SerializeField] Transform jogador; // o jogador a ser seguido
     private Rigidbody2D rb;
     private Vector2 movimento;
     [SerializeField] float moveSpeed = 5f;
+    //[SerializeField] float destroyDelay = 0.5f;
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
@@ -17,12 +18,24 @@ public class CarroPoliciaTesteMedium : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        teste();
+        Vector3 direcao = jogador.position - transform.position; // eu pego a direção do meu jogador
+        float angulo = Mathf.Atan2(direcao.y, direcao.x) * Mathf.Rad2Deg;
+        // a função acima retorna um valor em radiano mas tem q ser degrees, ent multi. pelo Rad2Deg, que faz isso
+        rb.rotation = angulo - 90f; //botei -90 pq o sprite tava ficando de lado, ai -90 bota na posição certa
+        direcao.Normalize(); // o valor tem q ser entre -1 e +1, o Normalize faz isso pelo q eu entendi
+        movimento = direcao;
     }
 
-    void teste()
+    private void FixedUpdate()
+    { // ele disse "já que estamos lidando com física(mov do carro) é bom fazer isso num FixedUpdate
+        moverCarro(movimento);
+    }
+
+    void moverCarro(Vector2 direcao)
     {
-        transform.position = Vector2.MoveTowards(transform.position, jogador.transform.position, moveSpeed * Time.deltaTime);
+        rb.MovePosition((Vector2)transform.position + (direcao * moveSpeed * Time.deltaTime));
+        //a linha acima pega a posição atual e mover na direção especificada com vel do carro junto com o delta time pra "ajeitar" as frames
+        //o (Vector2) ajusta para ser um vetor de 2 e não de 3 como tava dando erro sem ele, não entendi mt bem...
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -34,5 +47,5 @@ public class CarroPoliciaTesteMedium : MonoBehaviour
             WinLooseScript.perderMedium();
         }
     }
-}
 
+}
